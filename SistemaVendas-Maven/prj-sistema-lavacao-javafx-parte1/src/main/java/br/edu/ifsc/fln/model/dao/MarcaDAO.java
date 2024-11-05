04f3cd1499a35cacc4f45017f7d66a9ad612914c
mpisching <55263037+mpisching@users.noreply.github.com>
2024-11-05 17:41:33 -0300

    Exemplo de acesso a banco de dados

---
+++ SistemaVendas-Maven/prj-sistema-lavacao-javafx-parte1/src/main/java/br/edu/ifsc/fln/model/dao/MarcaDAO.java
@@ -0,0 +1,104 @@
+package br.edu.ifsc.fln.model.dao;
+
+import br.edu.ifsc.fln.model.domain.Marca;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MarcaDAO {
+
+    private Connection connection;
+    
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean inserir(Marca marca) {
+        String sql = "INSERT INTO marca(nome) VALUES(?)";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, marca.getNome());
+            stmt.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(MarcaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean alterar(Marca marca) {
+        String sql = "UPDATE marca SET nome=? WHERE id=?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, marca.getNome());
+            stmt.setInt(2, marca.getId());
+            stmt.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(MarcaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean remover(Marca marca) {
+        String sql = "DELETE FROM marca WHERE id=?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, marca.getId());
+            stmt.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(MarcaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public List<Marca> listar() {
+        String sql = "SELECT * FROM marca";
+        List<Marca> retorno = new ArrayList<>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultado = stmt.executeQuery();
+            while (resultado.next()) {
+                Marca marca = new Marca();
+                marca.setId(resultado.getInt("id"));
+                marca.setNome(resultado.getString("nome"));
+                retorno.add(marca);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MarcaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retorno;
+    }
+
+    public Marca buscar(Marca marca) {
+        Marca retorno = buscar(marca.getId());
+        return retorno;
+    }
+    
+    public Marca buscar(int id) {
+        String sql = "SELECT * FROM marca WHERE id=?";
+        Marca retorno = new Marca();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet resultado = stmt.executeQuery();
+            if (resultado.next()) {
+                retorno.setId(resultado.getInt("id"));
+                retorno.setNome(resultado.getString("nome"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MarcaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retorno;        
+    }
+}
