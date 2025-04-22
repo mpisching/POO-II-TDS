661328b09c0996606dfad86b8ed8ce0ff80d2556
mpisching <55263037+mpisching@users.noreply.github.com>
2025-04-22 17:39:24 -0300

    Aula 22/04/2025
    Projeto Lavação - parte 1

---
+++ SistemaVendas-Maven/prj-sistema-lavacao2025-javafx-parte1/src/main/java/br/edu/ifsc/fln/model/dao/CorDAO.java
@@ -0,0 +1,105 @@
+package br.edu.ifsc.fln.model.dao;
+
+import br.edu.ifsc.fln.model.domain.Cor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CorDAO {
+
+    private Connection connection;
+    
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean inserir(Cor cor) {
+        String sql = "INSERT INTO cor(nome) VALUES(?)";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, cor.getNome());
+            stmt.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(CorDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean alterar(Cor cor) {
+        String sql = "UPDATE cor SET descricao=? WHERE id=?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, cor.getDescricao());
+            stmt.setInt(2, cor.getId());
+            stmt.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(CorDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean remover(Cor cor) {
+        String sql = "DELETE FROM cor WHERE id=?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, cor.getId());
+            stmt.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(CorDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public List<Cor> listar() {
+        String sql = "SELECT * FROM cor";
+        List<Cor> retorno = new ArrayList<>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultado = stmt.executeQuery();
+            while (resultado.next()) {
+                Cor cor = new Cor();
+                cor.setId(resultado.getInt("id"));
+                cor.setDescricao(resultado.getString("descricao"));
+                retorno.add(cor);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retorno;
+    }
+
+    public Cor buscar(Cor cor) {
+        Cor retorno = buscar(cor.getId());
+        return retorno;
+    }
+    
+    public Cor buscar(int id) {
+        String sql = "SELECT * FROM cor WHERE id=?";
+        Cor retorno = new Cor();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet resultado = stmt.executeQuery();
+            if (resultado.next()) {
+                retorno.setId(resultado.getInt("id"));
+                retorno.setDescricao(resultado.getString("descricao"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retorno;        
+    }
+}
