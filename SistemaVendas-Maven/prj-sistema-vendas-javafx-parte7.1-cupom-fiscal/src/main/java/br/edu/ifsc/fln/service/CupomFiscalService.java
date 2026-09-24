5ba5177f84a5ddd19c651775b56ec68fb75f9592
mpisching <mpisching@gmail.com>
2026-09-24 20:02:12 -0300

    Cupom Fiscal - atualização logo ifsc - url relativa

--- SistemaVendas-Maven/prj-sistema-vendas-javafx-parte7.1-cupom-fiscal/src/main/java/br/edu/ifsc/fln/service/CupomFiscalService.java
+++ SistemaVendas-Maven/prj-sistema-vendas-javafx-parte7.1-cupom-fiscal/src/main/java/br/edu/ifsc/fln/service/CupomFiscalService.java
@@ -8,6 +8,7 @@ import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
+import java.io.InputStream;
 import java.net.URL;
 import java.sql.Connection;
 import java.util.HashMap;
@@ -20,12 +21,16 @@ public class CupomFiscalService {
             final Database database = DatabaseFactory.getDatabase("mysql");
             final Connection connection = database.conectar();
 
+            // Carrega a imagem a partir da pasta resources do projeto Maven
+            InputStream logoStream = CupomFiscalService.class.getResourceAsStream("/icon/IFSC_logo_vertical.png");
+
             URL url = CupomFiscalService.class.getResource("/report/cupom_fiscal_venda2.jasper");
             JasperReport jasperReport = (JasperReport) JRLoader.loadObject(url);
             Map<String, Object> parametros =
                     new HashMap<>();
 
             parametros.put("venda_id", vendaId);
+            parametros.put("LOGO_PARAM", logoStream);
 
             JasperPrint jasperPrint =
                     JasperFillManager.fillReport(
